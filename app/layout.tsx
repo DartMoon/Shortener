d20a4f07355e6c68ad69d5396106048679119646
DartMoon <storogenkokirill@gmail.com>
2024-09-11 13:53:06 +0300

    Add basic environment setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,29 @@
-import type {Metadata} from "next";
-import localFont from "next/font/local";
+import { ReactNode } from 'react';
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900',
 });
 
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900',
 });
 
 export const metadata: Metadata = {
-  title: "Shortener: Shortening your way",
-  description: "Carry big links in your pocket with Shortener, the URL minimizer. This app transforms cumbersome URLs into compact versions ideal for sharing and saving space. Navigate the web smoothly with Shortener"
+  title: 'Shortener: Shortening your way',
+  description:
+    'Carry big links in your pocket with Shortener, the URL minimizer. This app transforms cumbersome URLs into compact versions ideal for sharing and saving space. Navigate the web smoothly with Shortener',
 };
 
-export default function RootLayout({children}: Readonly<{ children: React.ReactNode }>) {
+export default function RootLayout({ children }: Readonly<{ children: ReactNode }>) {
   return (
     <html lang="en">
-    <body
-      className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-    >
-    {children}
-    </body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-
-
 export default function Home() {
   return (
     <div>
       <h1>Shortener: Shortening your way</h1>
-      <p>Carry big links in your pocket with Shortener, the URL minimizer. This app transforms cumbersome URLs into
-        compact versions ideal for sharing and saving space. Navigate the web smoothly with Shortener</p>
+      <p>
+        Carry big links in your pocket with Shortener, the URL minimizer. This app transforms
+        cumbersome URLs into compact versions ideal for sharing and saving space. Navigate the web
+        smoothly with Shortener
+      </p>
     </div>
   );
 }
